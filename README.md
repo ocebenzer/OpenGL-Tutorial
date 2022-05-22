4fd8f341fcffbaf803926248f406891d03d24e40
omer <omer.benzer@boun.edu.tr>
2022-05-22 10:36:53 +0300

    added texture

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-https://en.wikibooks.org/wiki/OpenGL_Programming#The_basics_arc
+### OpenGL
+
+Followed tutorial at: https://en.wikibooks.org/wiki/OpenGL_Programming#The_basics_arc
+
+Tutorial repository at: https://gitlab.com/wikibooks-opengl/modern-tutorials
+
+### Installation (Ubuntu/Debian)
+
+* Install required packages
+```
+sudo apt-get install build-essential libgl1-mesa-dev
+sudo apt-get install libglew-dev libsdl2-dev libsdl2-image-dev libglm-dev libfreetype6-dev
+apt-get install libglm-dev
+```
+* Check your OpenGL Installation
+```
+glxinfo | grep OpenGL
+```
+* Select a branch
+* make & run
+```
+make
+./opengl
+```
--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #include <glm/gtc/type_ptr.hpp>
 /* Using SDL2 for the base window and OpenGL context init */
 #include <SDL2/SDL.h>
+#include "SDL_image.h"
 
 // include, utility functions etc.
 #include "./common/shader_utils.h"
@@ -19,54 +20,86 @@ using namespace std;
 // global variables
 int screen_width=800, screen_height=600;
 GLuint program;
-GLuint vbo_cube_vertices;
+GLuint vbo_cube_vertices, vbo_cube_texcoords;
 GLuint ibo_cube_elements;
-GLint attribute_coord3d, attribute_v_color, uniform_fade;  /* Global */
-GLint uniform_m_transform;
-GLint uniform_mvp;
+GLuint texture_id;
+GLint attribute_coord3d, attribute_texcoord;
+GLint uniform_mvp, uniform_mytexture;
 
+SDL_Renderer* renderer;
+SDL_Texture* res_texture;
 
-struct attributes {
-  	GLfloat coord3d[3];
-	GLfloat v_color[3];
-};
 
 bool init_resources() {
-	struct attributes cube_vertices[] = {
+	GLfloat cube_texcoords[2*4*6] = {
 		// front
-		{{-1.0, -1.0,  1.0}, {1.0, 0.0, 0.0}},
-		{{1.0, -1.0,  1.0}, {0.0, 1.0, 0.0}},
-		{{1.0,  1.0,  1.0}, {0.0, 0.0, 1.0}},
-		{{-1.0,  1.0,  1.0}, {1.0, 1.0, 1.0}},
+		0.0, 0.0,
+		1.0, 0.0,
+		1.0, 1.0,
+		0.0, 1.0,
+	};
+	for (int i = 1; i < 6; i++) {
+		memcpy(&cube_texcoords[i*4*2], &cube_texcoords[0], 2*4*sizeof(GLfloat));
+	}
+	glGenBuffers(1, &vbo_cube_texcoords);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_texcoords);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_texcoords), cube_texcoords, GL_STATIC_DRAW);
+
+	GLfloat cube_vertices[] = {
+		// front
+		-1.0, -1.0,  1.0,
+		1.0, -1.0,  1.0,
+		1.0,  1.0,  1.0,
+		-1.0,  1.0,  1.0,
+		// top
+		-1.0,  1.0,  1.0,
+		1.0,  1.0,  1.0,
+		1.0,  1.0, -1.0,
+		-1.0,  1.0, -1.0,
 		// back
-		{{-1.0, -1.0, -1.0}, {1.0, 0.0, 0.0}},
-		{{1.0, -1.0, -1.0}, {1.0, 0.0, 0.0}},
-		{{1.0,  1.0, -1.0}, {0.0, 0.0, 1.0}},
-		{{-1.0,  1.0, -1.0}, {1.0, 1.0, 1.0}}
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0,  1.0, -1.0,
+		1.0,  1.0, -1.0,
+		// bottom
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0,  1.0,
+		-1.0, -1.0,  1.0,
+		// left
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0,  1.0,
+		-1.0,  1.0,  1.0,
+		-1.0,  1.0, -1.0,
+		// right
+		1.0, -1.0,  1.0,
+		1.0, -1.0, -1.0,
+		1.0,  1.0, -1.0,
+		1.0,  1.0,  1.0,
 	};
 	glGenBuffers(1, &vbo_cube_vertices);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_vertices);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
-	/* init_resources */
+
 	GLushort cube_elements[] = {
 		// front
-		0, 1, 2,
-		2, 3, 0,
-		// right
-		1, 5, 6,
-		6, 2, 1,
-		// back
-		7, 6, 5,
-		5, 4, 7,
-		// left
-		4, 0, 3,
-		3, 7, 4,
-		// bottom
-		4, 5, 1,
-		1, 0, 4,
+		0,  1,  2,
+		2,  3,  0,
 		// top
-		3, 2, 6,
-		6, 7, 3
+		4,  5,  6,
+		6,  7,  4,
+		// back
+		8,  9, 10,
+		10, 11,  8,
+		// bottom
+		12, 13, 14,
+		14, 15, 12,
+		// left
+		16, 17, 18,
+		18, 19, 16,
+		// right
+		20, 21, 22,
+		22, 23, 20,
 	};
 	glGenBuffers(1, &ibo_cube_elements);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_cube_elements);
@@ -74,13 +107,33 @@ bool init_resources() {
 
 	// shaders
 	GLint link_ok = GL_FALSE;
-	
 	GLuint vs, fs;
-	if ((vs = create_shader("triangle.v.glsl", GL_VERTEX_SHADER))   == 0) return false;
-	if ((fs = create_shader("triangle.f.glsl", GL_FRAGMENT_SHADER)) == 0) return false;
+	if ((vs = create_shader("obj.v.glsl", GL_VERTEX_SHADER))   == 0) return false;
+	if ((fs = create_shader("obj.f.glsl", GL_FRAGMENT_SHADER)) == 0) return false;
 	
-	program = glCreateProgram();
+	// textures
+	SDL_Surface* res_texture = IMG_Load("res_texture.png");
+	if (res_texture == NULL) {
+		cerr << "IMG_Load: " << SDL_GetError() << endl;
+		return false;
+	}
+	glGenTextures(1, &texture_id);
+	glBindTexture(GL_TEXTURE_2D, texture_id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, // target
+		0, // level, 0 = base, no minimap,
+		GL_RGBA, // internalformat
+		res_texture->w, // width
+		res_texture->h, // height
+		0, // border, always 0 in OpenGL ES
+		GL_RGBA, // format
+		GL_UNSIGNED_BYTE, // type
+		res_texture->pixels);
+	SDL_FreeSurface(res_texture);
 
+
+	// apply resources
+	program = glCreateProgram();
 
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
@@ -98,12 +151,6 @@ bool init_resources() {
 		cerr << "Could not bind attribute " << attribute_name << endl;
 		return false;
 	}
-	attribute_name = "v_color";
-	attribute_v_color = glGetAttribLocation(program, attribute_name);
-	if (attribute_v_color == -1) {
-		cerr << "Could not bind attribute " << attribute_name << endl;
-		return false;
-	}
 
 	const char* uniform_name;
 	uniform_name = "mvp";
@@ -113,10 +160,10 @@ bool init_resources() {
 		return 0;
 	}
 
-	uniform_name = "fade";
-	uniform_fade = glGetUniformLocation(program, uniform_name);
-	if (uniform_fade == -1) {
-		cerr << "Could not bind uniform " << uniform_name << endl;
+	attribute_name = "texcoord";
+	attribute_texcoord = glGetAttribLocation(program, attribute_name);
+	if (attribute_texcoord == -1) {
+		cerr << "Could not bind attribute " << attribute_name << endl;
 		return false;
 	}
 	
@@ -128,35 +175,40 @@ void render(SDL_Window* window) {
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	
 	glUseProgram(program);
-	glEnableVertexAttribArray(attribute_coord3d);
 
+	glActiveTexture(GL_TEXTURE0);
+	glUniform1i(uniform_mytexture, /*GL_TEXTURE*/0);
+	glBindTexture(GL_TEXTURE_2D, texture_id);
+
+	glEnableVertexAttribArray(attribute_coord3d);
+	// Describe our vertices array to OpenGL (it can't guess its format automatically)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_vertices);
 	glVertexAttribPointer(
-		attribute_coord3d,   // attribute
-		3,                   // number of elements per vertex, here (x,y,z)
-		GL_FLOAT,            // the type of each element
-		GL_FALSE,            // take our values as-is
-		sizeof(struct attributes), // next coord3d appears every 6 floats
-		(GLvoid*) 0          // offset of the first element
+		  attribute_coord3d, // attribute
+		  3,                 // number of elements per vertex, here (x,y,z)
+		  GL_FLOAT,          // the type of each element
+		  GL_FALSE,          // take our values as-is
+		  0,                 // no extra data between each position
+		  0                  // offset of first element
 	);
-	glEnableVertexAttribArray(attribute_v_color);
-	glEnableVertexAttribArray(attribute_coord3d);
+
+	glEnableVertexAttribArray(attribute_texcoord);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_texcoords);
 	glVertexAttribPointer(
-		attribute_v_color,      // attribute
-		3,                      // number of elements per vertex, here (r,g,b)
-		GL_FLOAT,               // the type of each element
-		GL_FALSE,               // take our values as-is
-		sizeof(struct attributes),    // next color appears every 6 floats
-		(GLvoid*) offsetof(struct attributes, v_color)  // offset of first element
+		attribute_texcoord, // attribute
+		2,                  // number of elements per vertex, here (x,y)
+		GL_FLOAT,           // the type of each element
+		GL_FALSE,           // take our values as-is
+		0,                  // no extra data between each position
+		0                   // offset of first element
 	);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_cube_elements);
 	int size;  glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);
 	glDrawElements(GL_TRIANGLES, size/sizeof(GLushort), GL_UNSIGNED_SHORT, 0);
-	
-	glDisableVertexAttribArray(attribute_v_color);
-	glDisableVertexAttribArray(attribute_coord3d);
 
+	glDisableVertexAttribArray(attribute_coord3d);
+	glDisableVertexAttribArray(attribute_texcoord);
 	SDL_GL_SwapWindow(window);
 }
 
@@ -168,53 +220,26 @@ void onResize(int width, int height) {
 
 void free_resources() {
 	glDeleteProgram(program);
- 	glDeleteBuffers(1, &vbo_cube_vertices);
- 	glDeleteBuffers(1, &ibo_cube_elements);
-}
-
-float aspectaxis() {
-	float outputzoom = 1.0f;
-	float aspectorigin = 16.0f / 9.0f;
-	int aspectconstraint = 1;
-	switch (aspectconstraint) {
-		case 1:
-			if ((screen_width / screen_height) < aspectorigin) {
-				outputzoom *= (((float)screen_width / screen_height) / aspectorigin);
-			}
-			else {
-				outputzoom *= ((float)aspectorigin / aspectorigin);
-			}
-		break;
-		case 2:
-			outputzoom *= (((float)screen_width / screen_height) / aspectorigin);
-		break;
-		default:
-			outputzoom *= ((float)aspectorigin / aspectorigin);
-	}
-	return outputzoom;
-}
-
-float recalculatefov() {
-	return 2.0f * glm::atan(glm::tan(glm::radians(45.0f / 2.0f)) / aspectaxis());
+	glDeleteBuffers(1, &vbo_cube_vertices);
+	glDeleteBuffers(1, &vbo_cube_texcoords);
+	glDeleteBuffers(1, &ibo_cube_elements);
+	glDeleteTextures(1, &texture_id);
 }
 
 void logic() {
 	// mvp projection matrix
 	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -4.0));
 	glm::mat4 view = glm::lookAt(glm::vec3(0.0, 2.0, 0.0), glm::vec3(0.0, 0.0, -4.0), glm::vec3(0.0, 1.0, 0.0));
-	glm::mat4 projection = glm::perspective(recalculatefov(), 1.0f * screen_width / screen_height, 0.1f, 10.0f);
+	glm::mat4 projection = glm::perspective(45.0f, 1.0f * screen_width / screen_height, 0.1f, 10.0f);
 
-	float angle = SDL_GetTicks() / 1000.0 * 45;  // 45° per second
-	glm::vec3 axis_y(0, 1, 0);
-	glm::mat4 anim = glm::rotate(glm::mat4(1.0f), glm::radians(angle), axis_y);
+	float angle = SDL_GetTicks() / 1000.0 * 15;  // base 15° per second
+	glm::mat4 anim = \
+    glm::rotate(glm::mat4(1.0f), glm::radians(angle)*3.0f, glm::vec3(1, 0, 0)) *  // X axis
+    glm::rotate(glm::mat4(1.0f), glm::radians(angle)*2.0f, glm::vec3(0, 1, 0)) *  // Y axis
+    glm::rotate(glm::mat4(1.0f), glm::radians(angle)*4.0f, glm::vec3(0, 0, 1));   // Z axis
 
 	glm::mat4 mvp = projection * view * model * anim;
   	glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
-
-	// alpha 0->1->0 every 2 seconds
-	float cur_fade = sinf(SDL_GetTicks() / 1000.0 * (2*PI) / 2) / 2 + 0.5;
-	glUseProgram(program);
-	glUniform1f(uniform_fade, 1);
 }
 
 void mainLoop(SDL_Window* window) {
@@ -269,8 +294,8 @@ int main(int argc, char* argv[]) {
 	if (!init_resources())
 		return EXIT_FAILURE;
 
+	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
-	//glDepthFunc(GL_LESS);
 	// Enable alpha
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
